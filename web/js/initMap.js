84b9c95ed9fa5436d4269d2b76e71b06d51613cf
Csaba Sandor <sandor.csaba@gmail.com>
2013-02-11 11:01:09 +0100

    -Temperature window -Cookie setting/unsetting -Fixed layer control -Added Google Hybrid layer

--- web/js/initMap.js
+++ web/js/initMap.js
@@ -1,14 +1,19 @@
 var map,googleLayer,tempLayer,tempPoints,markers;
 
 function init(){
+
+	$( document ).tooltip();
+
+	
     map = new L.Map('map', {
 		center: mapCenter,
 		zoom: initZoom
 	});
     googleSatLayer = new L.Google('SATELLITE');
-	map.addLayer(googleSatLayer);
+	//map.addLayer(googleSatLayer);
 	googleLayer = new L.Google('ROADMAP');
-	map.addLayer(googleLayer);
+	//map.addLayer(googleLayer);ű
+	googleHybridLayer = new L.Google('HYBRID');
 	tempLayer = L.tileLayer.wms(baseDomainUrl+mapserverExecutable+"?map="+mapFileLocation, {
 		layers:  'sweden_temps,sweden_temps_iso',
 		format:'image/png; mode=24bit',
@@ -35,14 +40,8 @@ function init(){
 	$.getJSON( baseDomainUrl+appBaseDir+"temps.json",
 			function(data){
 				tempPoints=L.geoJson(data, {
-					pointToLayer: function (feature, latlng) {
-						/*var myIcon = L.icon({
-							iconUrl: 'http://www.temperatur.nu/gmapv3/icons.php?temperatur='+feature.properties.temperature,
-							iconSize: [25, 25],
-							iconAnchor: [12,1],
-							popupAnchor: [0, -2]
-						});	*/			
-						return L.marker(latlng/*, {icon: myIcon}*/);
+					pointToLayer: function (feature, latlng) {		
+						return L.marker(latlng);
 					},
 				    onEachFeature: function (feature, layer) {
 				    	var popupContent=feature.properties.name+"<br/>"+feature.properties.temperature+" &#8451;";
@@ -53,6 +52,9 @@ function init(){
 	                          .openOn(map);
 	                    });
 	                    layer.on('mouseout', function(e){ map.closePopup(); });
+	                    layer.on('click', function(e){updateInfo(feature);
+	                    	}
+	                    );
 				    	markers.addLayer(layer);
 				    	
 	                    
@@ -80,18 +82,62 @@ function init(){
 				});
 				markers.on('clustermouseout', function(e){ map.closePopup(); });
 				var baseMaps = {
+						"Google Streets": googleLayer,
 						"Google Satellite": googleSatLayer,
-						"Google Streets": googleLayer
-					    
+						"Google Hybrid": googleHybridLayer
 					};
 
 				var overlayMaps = {
 				    "Heatmap": tempLayer
-				    ,"Temperature points": tempPoints
+				    ,"Temperature points": markers
 				};
 				var layers=L.control.layers(baseMaps, overlayMaps);
 				map.addControl(layers);
-				map.addControl(new L.Control.Permalink({text: '<img src="images/bookmark.png"/>', layers: layers, position:'bottomright'}));
+				map.addControl(new L.Control.Permalink({text: '<img title="Ger dig tillgång till en url som leder till aktuell vy" src="images/book.png"/>', layers: layers, position:'bottomright'}));
+				map.addControl(new L.Control.Attribution({prefix:'<a href="javascript:saveExtent()"><img id="setCookie" title="Sparar aktuell vy och gör sätter kartsidan till startsida på temperatur.nu" src="images/plus.png"/></a>', position:'bottomright'}));
+				map.addControl(new L.Control.Attribution({prefix:'<a href="javascript:unSetExtent()"><img id="unSetCookie" title="Tar bort sparad vy kartan som startsida." src="images/minus.png"/></a>', position:'bottomright'}));
+				loadExtent();
 			}
 	);
+}
+
+function updateInfo(feature){
+	$.ajax({
+        type: "GET",
+        url: "http://www.temperatur.nu/internal_sign.php?type=map&stad="+feature.properties.alias,
+        dataType: "xml",
+        success: function(data) {
+        	var graph=$(data).find("graph");
+    		$("#mapinfo").html(
+    				"<img src='"+graph[0].textContent+"' class='graph'/>" +
+    				"<div class='dataPane'>"+
+    				"<img title='Temperatur på den här platsen' src='images/temperature.png'/>"+feature.properties.temperature+"&#8451;"+
+    				"<br /><a href='javascript:map.panTo(["+feature.geometry.coordinates[1]+","+feature.geometry.coordinates[0]+"])'><img title='Zooma in "+feature.properties.name+"' id='zoomTo' src='images/location plus.png'/></a>"+
+    				"<br /><a href='javascript:map.fitBounds([[55.24,11.04],[69.13,24.26]])'><img title='Zooma ut och centrera kartan' id='zoomToExtent' src='images/picture.png'/></a>"+
+    				"</div>"+
+    				"<p>info@temperature.nu</p>"
+    		);
+    		$("#mapinfo").css("visibility","visible");
+    		$( "#mapinfo" ).dialog({title:feature.properties.name,resizable:false, position:{my:"right top", at:"right top+10%", of:"#map"}});
+        }
+	});	
+}
+
+
+function saveExtent(){
+	var sw="[["+map.getBounds()._southWest.lat+","+map.getBounds()._southWest.lng+"]";
+	var ne="["+map.getBounds()._northEast.lat+","+map.getBounds()._northEast.lng+"]]";
+	$.cookie("extent__", sw+","+ne,{ expires: 100, path: '/temps/' });
+}
+
+function loadExtent(){	
+	var extent=$.cookie("extent__");
+	if(eval(extent)){
+		map.fitBounds(eval(extent));
+	}	
+}
+
+
+function unSetExtent(){
+	$.removeCookie('extent__', { path: '/temps/' });
 }
